dc80e3e6ee5e7d7070c954173f2058f2076a8397
Joe Czarnecki <joe.czar@outlook.com>
2020-03-30 17:36:31 +0200

    added date and _id fields to comment schema in studio

--- studio/schemas/documents/comment.js
+++ studio/schemas/documents/comment.js
@@ -1,28 +1,38 @@
 export default {
-    name: 'comment',
-    type: 'document',
-    title: 'Comment',
-    fields: [
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Name'
-      },
-      {
-          name: 'email',
-          type: 'string',
-          title: 'E-Mail'
-      },
-      {
-        name: 'message',
-        type: 'text',
-        title: 'Message'
-      },
-      {
-          name: 'pagecontext',
-          type: 'string',
-          title: 'Page Context'
-      }
-    ]
-  }
+  name: 'comment',
+  type: 'document',
+  title: 'Comment',
+  fields: [
+    {
+      _id: 'id',
+      type: 'string',
+      title: 'ID'
+    },
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'email',
+      type: 'string',
+      title: 'E-Mail'
+    },
+    {
+      name: 'message',
+      type: 'text',
+      title: 'Message'
+    },
+    {
+      name: 'date',
+      type: 'string',
+      title: 'Date'
+    },
+    {
+      name: 'pagecontext',
+      type: 'string',
+      title: 'Page Context'
+    }
+  ]
+}
   
