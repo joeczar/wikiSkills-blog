e6c38b52270a057365a47a36c545ff3055dc81df
Joe Czarnecki <joe.czar@outlook.com>
2020-03-30 17:22:00 +0200

    added schema for comment in studio

---
+++ studio/schemas/documents/comment.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'comment',
+    type: 'document',
+    title: 'Comment',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+          name: 'email',
+          type: 'string',
+          title: 'E-Mail'
+      },
+      {
+        name: 'message',
+        type: 'text',
+        title: 'Message'
+      },
+      {
+          name: 'pagecontext',
+          type: 'string',
+          title: 'Page Context'
+      }
+    ]
+  }
+  
--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -1,6 +1,6 @@
 export default {
   widgets: [
-    { name: 'structure-menu' },
+    {name: 'structure-menu'},
     {
       name: 'project-info',
       options: {
@@ -33,15 +33,15 @@ export default {
             value: 'https://github.com/joeczar/wikiSkills-blog',
             category: 'Code'
           },
-          { title: 'Frontend', value: 'https://wikiSkills-blog.netlify.com', category: 'apps' }
+          {title: 'Frontend', value: 'https://wikiSkills-blog.netlify.com', category: 'apps'}
         ]
       }
     },
-    { name: 'project-users', layout: { height: 'auto' } },
+    {name: 'project-users', layout: {height: 'auto'}},
     {
       name: 'document-list',
-      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
-      layout: { width: 'medium' }
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
     }
   ]
 }
